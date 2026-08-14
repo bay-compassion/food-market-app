a2c65ef4f54f5987b79275bec4ce06ed5c643d78
Matt Strom <strom.matt@gmail.com>
2026-08-13 20:49:57 -0700

    chore: add Netlify plugin to Vite configuration
    - Integrated `@netlify/vite-plugin` into the Vite setup for enhanced Netlify-specific functionality.
    - Updated `package.json` and `package-lock.json` to include the plugin dependency.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
+import netlify from '@netlify/vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig } from 'vite';
@@ -7,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue(), vueJsx(), vueDevTools()],
+	plugins: [vue(), vueJsx(), vueDevTools(), netlify()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
